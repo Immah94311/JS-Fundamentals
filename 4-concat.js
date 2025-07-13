23848944ec76ed6420a5e4cec2b8fc7859646cc1
immanuel mwiti <immanuelmwiti373@gmail.com>
2025-07-13 15:36:18 +0300

    sixth commit

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,10 @@
-const arg1 = "C";
+const arg1 = "HBTN";
 const arg2 = "Cool";
 const arg3 = "undefined";
 const arg4 = "Python"
 const arg5 = "fun"
 
-console.log(`${arg1} is ${arg2}`)
+
 console.log(`${arg1} is ${arg3}`)
 console.log(`${arg3} is ${arg3}`)
 console.log(`${arg4} is ${arg5}`)
